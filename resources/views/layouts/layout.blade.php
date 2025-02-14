00f3a0f877e027fb77244e6f747737fe3141eaad
Jorge Juarez B <jorgejb234@gmail.com>
2025-02-13 20:50:43 -0600

    se agrego el header y navbar blades para separar el codigo con un poco mas de orden

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+{{-- en los include se ponen los layouts --}}
+        @include('layouts.header')
+
+<body>
+    <div id="app">                    
+
+        @include('layouts.navbar')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
